776ae13a4e3fcec7022486b783e1b09f67db2d54
Tyuiu.KonyushevskyAA@outlook.com <aleksej.konyushevskij@mail.ru>
2023-10-24 21:00:21 +0500

    Task5

---
+++ Tyuiu.KonyushevskyAA.Sprint3.Task5.V16.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KonyushevskyAA.Sprint3.Task5.V16.Lib;
+
+namespace Tyuiu.KonyushevskyAA.Sprint3.Task5.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            int x = 2;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+            double wait = 115.748;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
