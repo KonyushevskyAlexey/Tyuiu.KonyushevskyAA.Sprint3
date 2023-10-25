e0d49bb9b98307dc5791d1b087c676a81ecdeea6
Tyuiu.KonyushevskyAA@outlook.com <aleksej.konyushevskij@mail.ru>
2023-10-25 15:08:06 +0500

    Task7

---
+++ Tyuiu.KonyushevskyAA.Sprint3.Task7.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonyushevskyAA.Sprint3.Task7.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonyushevskyAA.Sprint3.Task7.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6dc0687-828b-44dd-9ffa-cbfe4483ccfa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
